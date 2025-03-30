5a148dc7574c607d19b35a2fb0e88d0fbe8cdb50
Oscar Garza <osgarzam@gmail.com>
2025-03-30 17:55:24 -0600

    Created components folder and Button component

---
+++ src/components/Button.component.tsx
@@ -0,0 +1,11 @@
+import './Button.component.style.css'
+
+const ButtonComponent = () => {
+	return (
+		<div>
+			<button className={'buttonStyle'}>Test</button>
+		</div>
+	)
+}
+
+export default ButtonComponent
