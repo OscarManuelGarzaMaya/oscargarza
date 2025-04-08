1c0ad01ac3c4f665d0c2dc79bfc760494201ecf4
Oscar Garza <osgarzam@gmail.com>
2025-04-08 16:15:17 -0600

    Working on title and profile pic layout

--- src/components/Hello/Hello.component.tsx
+++ src/components/Hello/Hello.component.tsx
@@ -2,10 +2,20 @@ import '../../assets/globals.css'
 import './Hello.component.style.css'
 import dictionary from '../../dictionary.json'
 
+import profilePic from '../../assets/images/perfil.png'
+
 const HelloComponent = () => {
 	return (
 		<div className="helloContainer">
-			<div className="genericStyles helloMessage">{dictionary.hello.title}</div>
+			<div className="profilePicContainer">
+				<img
+					src={profilePic}
+					alt="imagenDePerfil"
+				/>
+			</div>
+			<div className="titleContainer">
+				<div className="genericStyles helloMessage">{dictionary.hello.title}</div>
+			</div>
 		</div>
 	)
 }
