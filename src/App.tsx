316e78b254f13bab82aa804b264cd9962658830e
Oscar Garza <osgarzam@gmail.com>
2025-04-08 14:40:28 -0600

    Updated invokations and applied styles to the new App structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import logo from './logo.svg'
 import './App.css'
-import ButtonComponent from './components/Button.component'
+import ButtonComponent from './components/Button/Button.component'
+import HelloComponent from './components/Hello/Hello.component'
+import dictionary from './dictionary.json'
 
 function App() {
 	return (
 		<div className="App">
 			<header className="header">
-				<ButtonComponent />
-				<ButtonComponent />
-				<ButtonComponent />
-				<ButtonComponent />
+				<ButtonComponent text={dictionary.header.buttons.aboutMe} />
+				<ButtonComponent text={dictionary.header.buttons.projects} />
+				<ButtonComponent text={dictionary.header.buttons.studies} />
+				<ButtonComponent text={dictionary.header.buttons.contactMe} />
 			</header>
+			<HelloComponent />
 		</div>
 	)
 }
