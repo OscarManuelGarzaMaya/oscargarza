fac40f93f2a8fd83315c8e701cf80968d8f9f746
Oscar Garza <osgarzam@gmail.com>
2025-04-08 14:39:02 -0600

    Moved button component to his folder

---
+++ src/components/Button/Button.component.tsx
@@ -0,0 +1,12 @@
+import '../../assets/globals.css'
+import './Button.component.style.css'
+
+const ButtonComponent = ({ text }: { text: string }) => {
+	return (
+		<div className="buttonContainer">
+			<button className="genericStyles buttonStyle">{text}</button>
+		</div>
+	)
+}
+
+export default ButtonComponent
