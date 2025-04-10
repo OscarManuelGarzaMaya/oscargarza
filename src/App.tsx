54db2d104a5b528d7cf6de79b5e685d75b44fcbd
Oscar Garza <osgarzam@gmail.com>
2025-04-10 12:36:20 -0600

    Updated App layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
 import './App.css'
-
-// Components
-import ButtonComponent from './components/Button/Button.component'
-import HelloComponent from './components/Hello/Hello.component'
-
-import dictionary from './dictionary.json'
+import './assets/globals.css'
 
 // Assets
 import typing from './assets/gifs/typing.gif'
+import HeaderComponent from './components/Header/Header.component'
+import profilePic from './assets/images/perfil.png'
+
+// Misc
+import dictionary from './dictionary.json'
 
 function App() {
 	return (
 		<div className="App">
-			<header className="header">
-				<ButtonComponent text={dictionary.header.buttons.aboutMe} />
-				<ButtonComponent text={dictionary.header.buttons.projects} />
-				<ButtonComponent text={dictionary.header.buttons.studies} />
-				<ButtonComponent text={dictionary.header.buttons.contactMe} />
-			</header>
-			<HelloComponent />
+			<HeaderComponent />
+			<div className="profilePicContainer">
+				<img
+					src={profilePic}
+					alt="imagenDePerfil"
+					className="profilePic"
+				/>
+				<div className="profilePicBackground"></div>
+			</div>
+			<div className="titleContainer">
+				<div className="genericStyles titleMessage genericShadow">
+					{dictionary.hello.title}
+				</div>
+			</div>
 			<div className="typingContainer">
 				<img
 					src={typing}
@@ -26,6 +33,27 @@ function App() {
 					className="typing genericShadow"
 				/>
 			</div>
+			<div className="softwareDeveloperContainer genericStyles genericShadow">
+				<p>{dictionary.experience.description}</p>
+			</div>
+			<div style={{ backgroundColor: 'lightgray' }}>2</div>
+			{/* <div style={{ backgroundColor: 'lightblue' }}>3</div>
+			<div style={{ backgroundColor: 'lightgreen' }}>4</div>
+			<div style={{ backgroundColor: 'lightyellow' }}>5</div>
+			<div style={{ backgroundColor: 'lightcoral' }}>6</div>
+			<div style={{ backgroundColor: 'lightpink' }}>7</div>
+			<div style={{ backgroundColor: 'lightgray' }}>8</div>
+			<div style={{ backgroundColor: 'lightpink' }}>9</div>
+			<div style={{ backgroundColor: 'lightgray' }}>10</div>
+			<div style={{ backgroundColor: 'lightblue' }}>11</div>
+			<div style={{ backgroundColor: 'lightgreen' }}>12</div>
+			<div style={{ backgroundColor: 'lightyellow' }}>13</div>
+			<div style={{ backgroundColor: 'lightcoral' }}>14</div>
+			<div style={{ backgroundColor: 'lightpink' }}>15</div>
+			<div style={{ backgroundColor: 'lightgray' }}>16</div> */}
+			{/* <div className="softwareDeveloperContainer">
+				<p>Soy desarrallodaro web con experiencia en...</p>
+			</div> */}
 		</div>
 	)
 }
