3e9b1b59bf66f78c5b5a97cb6d547aba88485b63
Oscar Garza <osgarzam@gmail.com>
2026-01-05 17:31:40 -0600

    Finished first About Me section version

--- src/components/AboutMe/AboutMe.component.tsx
+++ src/components/AboutMe/AboutMe.component.tsx
@@ -1,11 +1,32 @@
 import '../../assets/globals.css'
 import './AboutMe.component.style.css'
 
+// Assets
+import aboutMe from '../../assets/images/softwareDev.svg'
+
 const AboutMeComponent = () => {
 	return (
 		<div className="AboutMeContainer">
 			<div className="aboutMeTitleContainer">
-				<h1 className='aboutMeTitle'>About Me</h1>
+				<h1 className="aboutMeTitle">About Me</h1>
+			</div>
+			<div className="aboutMeContentContainer">
+				<p>
+					I am seeking an opportunity to apply my technical knowledge and
+					professional experience in a dynamic work environment, while continuously
+					learning and improving through new challenges. I am focused on delivering
+					quality results and contributing to solutions that are reliable, scalable,
+					and user-oriented.
+					<br />
+					<br />I am motivated by growth, responsibility, and innovation, and I enjoy
+					working on projects that allow me to develop both technically and
+					professionally. My goal is to contribute value to a collaborative team
+					while expanding my skills and taking on new challenges.
+				</p>
+				<img
+					src={aboutMe}
+					alt="About me"
+				/>
 			</div>
 		</div>
 	)
