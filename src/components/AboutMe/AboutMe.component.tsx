ac67965222bac98e1cc9f1430043bf5912c17783
Oscar Garza <osgarzam@gmail.com>
2026-01-05 17:16:24 -0600

    Added About Me title

--- src/components/AboutMe/AboutMe.component.tsx
+++ src/components/AboutMe/AboutMe.component.tsx
@@ -2,7 +2,13 @@ import '../../assets/globals.css'
 import './AboutMe.component.style.css'
 
 const AboutMeComponent = () => {
-	return <div className="AboutMeContainer"></div>
+	return (
+		<div className="AboutMeContainer">
+			<div className="aboutMeTitleContainer">
+				<h1 className='aboutMeTitle'>About Me</h1>
+			</div>
+		</div>
+	)
 }
 
 export default AboutMeComponent
