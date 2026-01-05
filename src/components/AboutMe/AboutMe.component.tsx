0a7225471bcc3560c2553ecae3d108a3eff408a5
Oscar Garza <osgarzam@gmail.com>
2026-01-04 22:03:50 -0600

    Added AboutMe component

---
+++ src/components/AboutMe/AboutMe.component.tsx
@@ -0,0 +1,8 @@
+import '../../assets/globals.css'
+import './AboutMe.component.style.css'
+
+const AboutMeComponent = () => {
+	return <div className="AboutMeContainer"></div>
+}
+
+export default AboutMeComponent
