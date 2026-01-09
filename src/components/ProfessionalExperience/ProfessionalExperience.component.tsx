db219a1e9eb213f39ede075b97f6643d430056c3
Oscar Garza <osgarzam@gmail.com>
2026-01-09 17:31:17 -0600

    Added ProfessionalExperience component

---
+++ src/components/ProfessionalExperience/ProfessionalExperience.component.tsx
@@ -0,0 +1,35 @@
+// Styles
+import './ProfessionalExperience.component.style.css'
+
+const ProfessionalExperienceComponent = () => {
+	return (
+		<div className="proffesionalExperienceContainer">
+			<h1 className="sectionTitle">Professional Experience</h1>
+			<div className="cardsContainer">
+				<div className="card">
+					<img
+						src=""
+						alt="Enterprise Logo"
+					/>
+					<h2>test</h2>
+				</div>
+				<div className="card">
+					<img
+						src=""
+						alt="Enterprise Logo"
+					/>
+					<h2>test</h2>
+				</div>
+				<div className="card">
+					<img
+						src=""
+						alt="Enterprise Logo"
+					/>
+					<h2>test</h2>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ProfessionalExperienceComponent
