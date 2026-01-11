07e0e358ba84525f6369b607778c9a5b956db068
Oscar Garza <osgarzam@gmail.com>
2026-01-11 16:47:10 -0600

    Created Courses component

---
+++ src/components/Courses/Courses.component.tsx
@@ -0,0 +1,11 @@
+// Styles
+import './Courses.component.style.css'
+
+const CoursesComponent = () => {
+	return (
+		<div className="coursesContainer">
+		</div>
+	)
+}
+
+export default CoursesComponent
