7a04183532da9ad5b2c9b28e9bb51f3a69b442be
Oscar Garza <osgarzam@gmail.com>
2026-01-11 19:10:44 -0600

    Updated course component styles

--- src/components/Courses/Courses.component.tsx
+++ src/components/Courses/Courses.component.tsx
@@ -5,6 +5,11 @@ import './Courses.component.style.css'
 import leftsideChevron from '../../assets/images/LeftsideChevron.svg'
 import rightsideChevron from '../../assets/images/RightsideChevron.svg'
 import ReactLogo from '../../assets/images/courses/React.svg'
+import HTMLLogo from '../../assets/images/courses/HTMLLogo.svg'
+import CSSLogo from '../../assets/images/courses/CSSLogo.svg'
+import JSLogo from '../../assets/images/courses/JSLogo.svg'
+import TSLogo from '../../assets/images/courses/TSLogo.svg'
+import mySQLLogo from '../../assets/images/courses/mySQLLogo.svg'
 
 const CoursesComponent = () => {
 	return (
@@ -25,36 +30,36 @@ const CoursesComponent = () => {
 					</div>
 					<div className="courseContainer">
 						<img
-							src={ReactLogo}
-							alt="React logo"
+							src={HTMLLogo}
+							alt="HTML logo"
+						/>
+						<p className="">HTML</p>
+					</div>
+					<div className="courseContainer">
+						<img
+							src={CSSLogo}
+							alt="CSS logo"
+						/>
+						<p className="">CSS</p>
+					</div>
+					<div className="courseContainer">
+						<img
+							src={JSLogo}
+							alt="JS logo"
+						/>
+						<p className="">Java Script</p>
+					</div>
+					<div className="courseContainer">
+						<img
+							src={TSLogo}
+							alt="Typescript logo"
 						/>
 						<p className="">React Native</p>
 					</div>
 					<div className="courseContainer">
 						<img
-							src={ReactLogo}
-							alt="React logo"
-						/>
-						<p className="">React Native</p>
-					</div>
-					<div className="courseContainer">
-						<img
-							src={ReactLogo}
-							alt="React logo"
-						/>
-						<p className="">React Native</p>
-					</div>
-					<div className="courseContainer">
-						<img
-							src={ReactLogo}
-							alt="React logo"
-						/>
-						<p className="">React Native</p>
-					</div>
-					<div className="courseContainer">
-						<img
-							src={ReactLogo}
-							alt="React logo"
+							src={mySQLLogo}
+							alt="mySQL logo"
 						/>
 						<p className="">React Native</p>
 					</div>
