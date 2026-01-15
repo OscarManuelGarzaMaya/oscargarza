71758ccb6e84c6bd5d812cb74a69f3e30c9eecda
Oscar Garza <osgarzam@gmail.com>
2026-01-15 17:37:46 -0600

    Invoked Education component and got rid of unused constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-
 import './App.css'
 import './assets/globals.css'
-
-// Assets
-import placeholder from './assets/images/HeroImage.svg'
 
 // Components
 import HeaderComponent from './components/Header/Header.component'
@@ -12,57 +7,9 @@ import HeroComponent from './components/Hero/Hero.component'
 import AboutMeComponent from './components/AboutMe/AboutMe.component'
 import ProfessionalExperienceComponent from './components/ProfessionalExperience/ProfessionalExperience.component'
 import CoursesComponent from './components/Courses/Courses.component'
+import EducationComponent from './components/Education/Education.component'
 
 function App() {
-	const [titles, setTitles] = useState([
-		{
-			id: 1,
-			title: 'GIT+GitHub: Todo un sistema de control de versiones de cero',
-			description:
-				'Aprendí la gestion de proyectos y manejo de historicos del proyecto.',
-			image: placeholder,
-		},
-		{
-			id: 2,
-			title: 'GIT+GitHub: Todo un sistema de control de versiones de cero',
-			description:
-				'Aprendí la gestion de proyectos y manejo de historicos del proyecto.',
-			image: placeholder,
-		},
-		{
-			id: 1,
-			title: 'GIT+GitHub: Todo un sistema de control de versiones de cero',
-			description:
-				'Aprendí la gestion de proyectos y manejo de historicos del proyecto.',
-			image: placeholder,
-		},
-		{
-			id: 2,
-			title: 'GIT+GitHub: Todo un sistema de control de versiones de cero',
-			description:
-				'Aprendí la gestion de proyectos y manejo de historicos del proyecto.',
-			image: placeholder,
-		},
-		{
-			id: 1,
-			title: 'GIT+GitHub: Todo un sistema de control de versiones de cero',
-			description:
-				'Aprendí la gestion de proyectos y manejo de historicos del proyecto.',
-			image: placeholder,
-		},
-		{
-			id: 2,
-			title: 'GIT+GitHub: Todo un sistema de control de versiones de cero',
-			description:
-				'Aprendí la gestion de proyectos y manejo de historicos del proyecto.',
-			image: placeholder,
-		},
-	])
-
-	const titleOrganizer = () => {
-		return 0
-	}
-
 	return (
 		<div className="App">
 			<HeaderComponent />
@@ -70,6 +17,7 @@ function App() {
 			<AboutMeComponent />
 			<ProfessionalExperienceComponent />
 			<CoursesComponent />
+			<EducationComponent />
 		</div>
 	)
 }
