065ac2041f18f071ee68d93889d549ea57dc9493
Oscar Garza <osgarzam@gmail.com>
2026-01-15 17:37:18 -0600

    Added Education component

---
+++ src/components/Education/Education.component.tsx
@@ -0,0 +1,31 @@
+// Styles
+import './Education.component.style.css'
+
+const EducationComponent = () => {
+	return (
+		<div className="educationContainer">
+			<h1 className="sectionTitle">Education</h1>
+			<div className="contentContainer">
+				<div className="collageContainer">
+					<h3 className="collegeTitle">Universidad del Noreste</h3>
+					<h4 className="careerTitle">Computer Systems and Electronics Engineer</h4>
+					<p className="educationDate">Tampico, México 2018-2020</p>
+					<p className="educationDescription">
+						I represented the University of the Northeast at the State Contest of
+						Creativity and Innovation in Technology, ExpoCiencias Tamaulipas.
+					</p>
+				</div>
+				<hr />
+				<div className="collageContainer">
+					<h3 className="collegeTitle">
+						Science and Advance Studies Institute of Tamaulipas
+					</h3>
+					<h4 className="careerTitle">Computer Systems and Electronics Engineer</h4>
+					<p className="educationDate">Tampico, México 2018-2020</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default EducationComponent
