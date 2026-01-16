e499b2780520b6d94778b96d7b7b102117be6669
Oscar Garza <osgarzam@gmail.com>
2026-01-16 04:54:24 -0600

    Deleted dictionary file

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -5,15 +5,12 @@ import './Header.component.style.css'
 import DropdownComponent from '../Dropdown/Dropdown.component'
 import ProfilesComponent from '../Profiles/Profiles.component'
 
-// Misc
-import dictionary from '../../dictionary.json'
-
 const HeaderComponent = () => {
 	return (
 		<section className="headerSiteContainer">
 			<div className="headerInnerContainer">
 				<div className="leftContainer">
-					<h1 className="headerName">{dictionary.header.name}</h1>
+					<h1 className="headerName">Oscar Garza</h1>
 				</div>
 				<div className="rightContainer">
 					<DropdownComponent />
