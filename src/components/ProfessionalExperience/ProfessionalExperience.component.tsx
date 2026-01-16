b32ce456f30c47bf664f797530b9ee6b6b0eda6d
Oscar Garza <osgarzam@gmail.com>
2026-01-16 04:53:49 -0600

    Added translation fro ProfessionalExperience section

--- src/components/ProfessionalExperience/ProfessionalExperience.component.tsx
+++ src/components/ProfessionalExperience/ProfessionalExperience.component.tsx
@@ -1,54 +1,74 @@
 // Styles
 import './ProfessionalExperience.component.style.css'
 
-// Misc
-import dictionary from '../../dictionary.json'
-
 // Assets
 import firstEnterpriseLogo from '../../assets/images/professionalExperiences/firstEnterpriseLogo.svg'
 import secondEnterpriseLogo from '../../assets/images/professionalExperiences/secondEnterpriseLogo.svg'
 import thirdEnterpriseLogo from '../../assets/images/professionalExperiences/thirdEnterpriseLogo.svg'
 import CardsComponent from '../Cards/Cards.component'
 
+// Librry
+import { useTranslation } from 'react-i18next'
+
+// TODO: Make sure texts are genuinely translated from CV file
 const ProfessionalExperienceComponent = () => {
+	const { t } = useTranslation()
+
 	return (
 		<section className="proffesionalExperienceContainer">
-			<h1 className="sectionTitle">{dictionary.proffesionalExperience.title}</h1>
+			<h1 className="sectionTitle">{t(['professionalExperience.title'])}</h1>
 			<div className="cardsContainer">
 				<CardsComponent
 					enterpriseLogo={firstEnterpriseLogo}
 					enterpriseTitle="SOLMETI"
-					positionTitle="Web Developer"
-					date="April 2025 - Present"
+					positionTitle={t(['professionalExperience.jobs.SOLEMTI.position'])}
+					date={t(['professionalExperience.jobs.SOLEMTI.date'])}
 				>
 					<p>
-						Developed custom client-side solutions using <b>REACT.</b> <br />
-						Implemented mono-repo structures to improve scalability and maintenance.
+						{t(['professionalExperience.jobs.SOLEMTI.description.firstParagraph'])}
+						<b>REACT.</b> <br />
+						{t(['professionalExperience.jobs.SOLEMTI.description.secondParagraph'])}
 					</p>
 				</CardsComponent>
 				<CardsComponent
 					enterpriseLogo={secondEnterpriseLogo}
 					enterpriseTitle="RESSER"
-					positionTitle="Web Developer (Full-Stack)"
-					date="Aug 2023 - Aug 2024"
+					positionTitle={t(['professionalExperience.jobs.RESSER.position'])}
+					date={t(['professionalExperience.jobs.RESSER.date'])}
 				>
 					<p>
-						Developed internal systems using <b>React, C#</b> and <b>SQL</b>. <br />
-						Proposed and implemented Smart Components standards for large-scale
-						projects.
+						{t(['professionalExperience.jobs.RESSER.description.firstParagraph'])}
+						<b>React, C#</b>
+						{t(['professionalExperience.jobs.RESSER.description.secondParagraph'])}
+						<b>SQL</b>. <br />
+						{t(['professionalExperience.jobs.RESSER.description.thirdParagraph'])}
 					</p>
 				</CardsComponent>
 				<CardsComponent
 					enterpriseLogo={thirdEnterpriseLogo}
-					enterpriseTitle="IO CONNECT SERVICES"
-					positionTitle="Web & Mobile App Developer"
-					date="Oct 2021 - April 2023"
+					enterpriseTitle={'IO CONNECT SERVICES'}
+					positionTitle={t([
+						'professionalExperience.jobs.ioConnectServices.position',
+					])}
+					date={t(['professionalExperience.jobs.ioConnectServices.date'])}
 				>
 					<p>
-						Maintained and updated enterprise websites using <b>Angular</b> and{' '}
-						<b>PHP.</b>
-						<br />
-						Developed mobile solutions with <b>React Native</b> for a banking startup.
+						{t([
+							'professionalExperience.jobs.ioConnectServices.description.firstParagraph',
+						])}
+						<b>Angular</b>
+						{t([
+							'professionalExperience.jobs.ioConnectServices.description.secondParagraph',
+						])}
+						<b>PHP. </b>
+						{/* <br /> */}
+						{t([
+							'professionalExperience.jobs.ioConnectServices.description.thirdParagraph',
+						])}
+						<b>React Native</b>
+						{t([
+							'professionalExperience.jobs.ioConnectServices.description.forthParagraph',
+						])}
 					</p>
 				</CardsComponent>
 			</div>
