69e0c7a0a3d059dbc5741b9e7b4bf75a53099ba1
Oscar Garza <osgarzam@gmail.com>
2026-01-17 18:11:02 -0600

    Added algorithm to select a language

--- src/components/Dropdown/Dropdown.component.tsx
+++ src/components/Dropdown/Dropdown.component.tsx
@@ -11,8 +11,45 @@ import franceFlag from '../../assets/images/flags/france-flag.svg'
 // Icons
 import downsideChevron from '../../assets/images/DownsideChevron.svg'
 
+// Libraries
+import { useTranslation } from 'react-i18next'
+
 const DropdownComponent = () => {
+	const { i18n } = useTranslation()
+
 	const [open, setOpen] = useState(false)
+
+	const [languageList, setLangugeList] = useState<
+		Array<{
+			acronim: string
+			flag: string
+			isSelected: boolean
+			language: string
+			name: string
+		}>
+	>([
+		{
+			acronim: 'EN',
+			flag: americanFlag,
+			isSelected: true,
+			language: 'en',
+			name: 'English',
+		},
+		{
+			acronim: 'SP',
+			flag: mexicanFlag,
+			isSelected: false,
+			language: 'es',
+			name: 'Spanish',
+		},
+		{
+			acronim: 'FR',
+			flag: franceFlag,
+			isSelected: false,
+			language: 'fr',
+			name: 'French',
+		},
+	])
 
 	return (
 		<div className="langDropdown">
@@ -20,42 +57,48 @@ const DropdownComponent = () => {
 				className="langTrigger"
 				onClick={() => setOpen(!open)}
 			>
-				<div className="langLabel">
-					<span>SP</span>
-					<img
-						src={mexicanFlag}
-						alt="Spanish"
-					/>
-				</div>
-				<img
-					src={downsideChevron}
-					alt="chevron"
-					className="downsideChevronImg"
-				/>
+				{languageList
+					.filter((lang) => lang.isSelected)
+					.map((lang) => (
+						<>
+							<div className="langLabel">
+								<span>{lang.acronim}</span>
+								<img
+									src={lang.flag}
+									alt={lang.name}
+								/>
+							</div>
+							<img
+								alt="chevron"
+								className="downsideChevronImg"
+								src={downsideChevron}
+							/>
+						</>
+					))}
 			</button>
 			<ul className={`languageMenu ${open ? 'open' : ''}`}>
-				<li>
-					{/* 
-						Todo: Add an algorithm to split the text on max of 3 chars and display full world on a tooltip
-					*/}
-					<span>ENG</span>
-					<img
-						src={americanFlag}
-						alt="English"
-						className="flagIcon"
-					/>
-				</li>
-				{/* 
-					Todo: Add algorithm to display this divider between all items but above the first one and be need the last one
-				*/}
-				<hr className="langDivider" />
-				<li>
-					<span>FR</span>
-					<img
-						src={franceFlag}
-						alt="French"
-					/>
-				</li>
+				{languageList
+					.filter((lang) => !lang.isSelected)
+					.map((lang) => (
+						<li
+							title={lang.name}
+							onClick={() => {
+								i18n.changeLanguage(lang.language)
+								setLangugeList((currentLangs) =>
+									currentLangs.map((currentLang) => ({
+										...currentLang,
+										isSelected: currentLang.language === lang.language,
+									})),
+								)
+							}}
+						>
+							<span>{lang.acronim}</span>
+							<img
+								alt={lang.name}
+								src={lang.flag}
+							/>
+						</li>
+					))}
 			</ul>
 		</div>
 	)
