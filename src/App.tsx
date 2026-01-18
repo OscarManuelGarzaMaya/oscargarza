672433371ae47a92f8ed761a37b5128e91974fb9
Oscar Garza <osgarzam@gmail.com>
2026-01-18 14:12:27 -0600

    Installed library and algorithm to change language to browser language on page load

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import FooterComponent from './components/Footer/Footer.component'
 // Libraries
 import i18next from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 // Assets
 import en from './locales/en/translation.json'
@@ -21,15 +22,23 @@ import es from './locales/es/translation.json'
 import fr from './locales/fr/translation.json'
 
 function App() {
-	i18next.use(initReactI18next).init({
-		fallbackLng: 'en',
-		resources: {
-			en: { translation: en },
-			es: { translation: es },
-			fr: { translation: fr },
-		},
-		interpolation: { escapeValue: false },
-	})
+	i18next
+		.use(LanguageDetector)
+		.use(initReactI18next)
+		.init({
+			fallbackLng: 'en',
+			resources: {
+				en: { translation: en },
+				es: { translation: es },
+				fr: { translation: fr },
+			},
+			supportedLngs: ['en', 'fr', 'es'],
+			interpolation: { escapeValue: false },
+			detection: {
+				order: ['navigator'],
+				caches: [],
+			},
+		})
 
 	return (
 		<div className="App">
