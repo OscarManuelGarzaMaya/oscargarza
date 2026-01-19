48707e7711710ce7d07cafd5f3a1827259a85c67
Oscar Garza <osgarzam@gmail.com>
2026-01-18 19:28:03 -0600

    Updates

---
+++ .fttemplates/[FTName]Component/[FTName].component.tsx
@@ -0,0 +1,11 @@
+// Styles
+import './[FTName].component.style.css'
+
+const [FTName]Component = () => {
+	return (
+		<div className="[FTName | camelcase]Container">
+		</div>
+	)
+}
+
+export default [FTName]Component
