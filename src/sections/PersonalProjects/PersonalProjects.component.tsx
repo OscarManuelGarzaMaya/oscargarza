4aaaa9cbbb2f722c1ea7b294a84e1a55d83bdb53
Oscar Garza <osgarzam@gmail.com>
2026-07-31 19:39:11 -0600

    Created PersonalProjects component section

---
+++ src/sections/PersonalProjects/PersonalProjects.component.tsx
@@ -0,0 +1,30 @@
+// Styles
+import CardComponent from '../../components/Card/Card.component'
+import './PersonalProjects.component.style.css'
+
+// Assets
+import ezPDFLogo from '../../assets/images/professionalExperiences/ezPdf_logo.png'
+
+const PersonalProjectsComponent = () => {
+	return (
+		<section className="personalProjectsContainer">
+			<h1 className="sectionTitle">{'Proyectos personales'}</h1>
+			<div className="projectsContainer">
+				<CardComponent
+					title="EZ-PDF"
+					logo={ezPDFLogo}
+					heading={"test"}
+					date={"test"}
+				>
+					<ul>
+						<li>
+							<p>{}</p>
+						</li>
+					</ul>
+				</CardComponent>
+			</div>
+		</section>
+	)
+}
+
+export default PersonalProjectsComponent
