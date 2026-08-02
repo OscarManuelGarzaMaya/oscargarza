268c1b964867f7901389efc344cbd526feacbcc1
Oscar Garza <osgarzam@gmail.com>
2026-08-02 14:25:35 -0600

    Created template for sections

---
+++ .fttemplates/[FTName]Section/[FTName].section.tsx
@@ -0,0 +1,11 @@
+// Styles
+import './[FTName].section.style.css'
+
+const [FTName]Section = () => {
+	return (
+		<div className="[FTName | camelcase]Container">
+		</div>
+	)
+}
+
+export default [FTName]Section
